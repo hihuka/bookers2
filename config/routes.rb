c1699e6e89cd1bb64198434ba059dc1674a770c2
EC2 Default User <ec2-user@ip-172-31-16-117.ec2.internal>
2022-08-11 05:24:50 +0000

    bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   get 'homes/top'
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
   resources :users, only: [:show, :edit, :index]
-  resources :user_images, only: [:new, :create, :index, :show]
   patch 'users/:id' => 'users#update', as: 'update_user'
 end
