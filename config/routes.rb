e0e6be985194fa3dfe9f0703b93ee25edde0c713
EC2 Default User <ec2-user@ip-172-31-16-117.ec2.internal>
2022-08-13 08:52:59 +0000

    bookers2課題

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get 'homes/top'
+  get 'homes/about'
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
   resources :users, only: [:show, :edit, :index]
   patch 'users/:id' => 'users#update', as: 'update_user'
