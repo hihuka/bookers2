8c06c2e75718006c46ae392e192a590c140a319b
EC2 Default User <ec2-user@ip-172-31-16-117.ec2.internal>
2022-08-27 08:34:12 +0000

    bookers2課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,7 +20,7 @@ class BooksController < ApplicationController
    @book = Book.new(book_params)
    @book.user_id = current_user.id
    if @book.save
-     redirect_to book_path(@book.id)
+     redirect_to book_path(@book.id), flash: {success: "You have created book successfully."}
    else
      @books = Book.all
      @user = current_user
@@ -40,7 +40,7 @@ class BooksController < ApplicationController
   def update
    @book = Book.find(params[:id])
     if @book.update(book_params)
-     redirect_to book_path(@book.id)
+     redirect_to book_path(@book.id), flash: {success: "You have updated book successfully."}
     else
      render :edit
     end
@@ -55,6 +55,6 @@ class BooksController < ApplicationController
     private
 
   def book_params
-    params.require(:book).permit(:title, :book)
+    params.require(:book).permit(:title, :body)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_074422) do
+ActiveRecord::Schema.define(version: 2022_08_08_054118) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -42,10 +42,10 @@ ActiveRecord::Schema.define(version: 2022_08_25_074422) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
+    t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
-    t.string "body"
   end
 
   create_table "users", force: :cascade do |t|
